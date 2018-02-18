56e7aed5bba504972349a2536b20d6cd2691696f
Blake Regalia <blake.regalia@gmail.com>
2018-02-18 10:49:52 -0800

    base

---
+++ lib/sql/add-indexes.sql
@@ -0,0 +1,16 @@
+
+create unique index idx_node_essence_ns on nodes(ntype, svalue)
+	where ltype is null
+		and lang is null;
+
+create unique index idx_node_essence_gs on nodes(lang, svalue)
+	where ntype = 'string'
+		and ltype is null
+		and lang is not null;
+
+create unique index idx_node_essence_nls on nodes(ntype, ltype, svalue)
+	where ltype is not null
+		and lang is null;
+
+
+alter table triples add constraint triples_unique unique(subject, predicate, object);
